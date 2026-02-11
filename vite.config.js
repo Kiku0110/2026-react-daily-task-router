fa61be3c60177c3181f7dfe45ff7727e96cda902
Kiku <likusa0110@gmail.com>
2026-02-11 18:38:23 +0800

    新增gh-pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // eslint-disable-next-line no-undef
+  base:process.env.NODE_ENV === 'production'? '/2026-react-daily-task-router/':'/',  
   plugins: [react()],
 })
